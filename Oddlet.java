efacdf5b68ae7047d674363f72428ad3e10fe4f3
BharkaviS <sashibharkavi@gmail.com>
2020-01-30 22:32:42 +0530

    commiting

---
+++ Oddlet.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Oddlet{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String s;
+    int c=0;
+    s=sc.nextLine();
+    int[] a = new int[256];
+    for(int i =0;i<s.length();i++){
+      a[s.charAt(i)]++;
+    }
+    for(int i=0;i<s.length();i++){
+      if((a[s.charAt(i)])%2==1){
+        System.out.print(s.charAt(i));
+        a[s.charAt(i)]=0;
+        c=1;
+      }
+    }
+    if(c==0){
+      System.out.println("-1");
+    }  }
+}
