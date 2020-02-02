92a70b13575aee472e4ce4aa21f0c88a85c72fce
BharkaviS <sashibharkavi@gmail.com>
2020-02-02 10:52:59 +0530

    commit

--- Rankrevsub.py
+++ Rankrevsub.py
@@ -1,11 +1,11 @@
 test=input("Enter the input:")
-test_str=test[::-1]
+#test=test[::-1]
 v=test
 a = [test[i: j] for i in range(len(test))
           for j in range(i + 1, len(test) + 1)]
 
 a.sort()
-#a=list(dict.fromkeys(a))
+a=list(dict.fromkeys(a))
 for i in a:
     if(i==v):
         print("The rank is: ",a.index(i)+1)
