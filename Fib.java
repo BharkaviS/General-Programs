f834a43a86a70c52343f652f6d82540c915437f5
BharkaviS <sashibharkavi@gmail.com>
2020-02-09 22:05:37 +0530

    Fibonnaci(DP)

---
+++ Fib.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Fib{
+
+  public int Ugly(int n){
+    int[] F = new int[n+2];
+    int i;
+    F[0]=0;
+    F[1]=1;
+    for(i=2;i<=n;i++){
+      F[i]=F[i-1]+F[i-2];
+    }
+      return F[n];
+  }
+  public static void main(String args[]){
+    Fib u = new Fib();
+    Scanner sc = new Scanner(System.in);
+    int n,res;
+    n=sc.nextInt();
+    System.out.println(u.Ugly(n));
+}
+}
